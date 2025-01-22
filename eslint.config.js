99d58f600dcc98bf07b99071813aafd4055f28e9
Ammar Yaser <technohat.dev@gmail.com>
2025-01-22 23:43:22 +0200

    Add Home component and configure Tailwind CSS styles

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,22 @@
+import react from "eslint-plugin-react";
 import tseslint from "typescript-eslint";
 
 import eslint from "@eslint/js";
 
 export default tseslint.config(
 	eslint.configs.recommended,
-	tseslint.configs.recommended,
+	tseslint.configs.recommendedTypeChecked,
 	tseslint.configs.strict,
 	tseslint.configs.stylistic,
 	{
-		ignores: ["eslint.config.js", "tailwind.config.ts"],
+		ignores: ["eslint.config.js", "tailwind.config.js"],
+		settings: { react: { version: "18.3" } },
+		plugins: {
+			react
+		},
 		languageOptions: {
 			parserOptions: {
-				projectService: true,
+				project: ["./tsconfig.node.json", "./tsconfig.app.json"],
 				tsconfigRootDir: import.meta.dirname
 			}
 		}
@@ -22,7 +27,9 @@ export default tseslint.config(
 			"prefer-const": "warn",
 			"array-callback-return": "error",
 			"no-restricted-imports": ["error", { patterns: ["../"] }],
-			"no-console": ["error", { allow: ["warn", "error"] }]
+			"no-console": ["error", { allow: ["warn", "error"] }],
+			...react.configs.recommended.rules,
+			...react.configs["jsx-runtime"].rules
 		}
 	},
 	{
@@ -37,6 +44,7 @@ export default tseslint.config(
 			"@typescript-eslint/no-mixed-enums": "warn",
 			"@typescript-eslint/no-unnecessary-condition": "warn",
 			"@typescript-eslint/prefer-enum-initializers": "warn",
+			"@typescript-eslint/no-non-null-assertion": "off",
 			"@typescript-eslint/naming-convention": [
 				"error",
 				{
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				primary: "SignikaNegative"
+			},
+			colors: {
+				primary: "#0873d7"
+			}
+		}
 	},
 	plugins: []
 };
